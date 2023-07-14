099721fe1fe42a1f401ec05e7fb6ba19b9641876
TheTeaCup <lw0482352@gmail.com>
2023-07-14 15:47:03 -0400

    SEO

---
+++ next-seo.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'Hunter Wilson',
+    titleTemplate: "%s | Hunter Wilson",
+    description: 'A list of links to my social media and other websites.',
+    siteUrl: "https://links.hunterwilson.dev",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '@/styles/globals.css'
 import {ChakraProvider} from "@chakra-ui/react";
 import theme from "@/utils/theme";
 import Script from "next/script";
+import {DefaultSeo} from "next-seo";
+import SEO from "@/components/seo";
 
 export default function App({Component, pageProps}) {
     return (
         <>
+            <DefaultSeo {...SEO}/>
             <Script async defer data-website-id="46f0a41b-d1b2-4762-a1e0-44b254a0d8d3"
                     src="https://analytics.theteacup.dev/umami.js"></Script>
             <ChakraProvider theme={theme}>
