13a87ebc5bbb9c55d3e0032d5c5e55b8a9cf65ab
TheTeaCup <lw0482352@gmail.com>
2023-07-14 15:50:42 -0400

    Fix build error

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import {Head, Html, Main, NextScript} from 'next/document'
 import {ColorModeScript} from "@chakra-ui/react";
+import theme from "@/utils/theme";
 
 export default function Document() {
     return (
