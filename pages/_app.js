e63f9782ba8e8d98727ff6f432ecbdf9a505f5d4
TheTeaCup <lw0482352@gmail.com>
2023-07-14 15:44:47 -0400

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import {ChakraProvider} from "@chakra-ui/react";
 import theme from "@/utils/theme";
+import Script from "next/script";
 
 export default function App({Component, pageProps}) {
-  return (
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-  )
+    return (
+        <>
+            <Script async defer data-website-id="46f0a41b-d1b2-4762-a1e0-44b254a0d8d3"
+                    src="https://analytics.theteacup.dev/umami.js"></Script>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
+    )
 }
