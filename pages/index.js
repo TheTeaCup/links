a7f3108f648a29a4947a4fe58ed629bedae5a4c8
TheTeaCup <lw0482352@gmail.com>
2023-07-14 23:08:55 -0400

    Home page update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
-import {Heading} from "@chakra-ui/react";
+import {Avatar, Box, Center, Container, Heading, Stack, Text} from "@chakra-ui/react";
 import Layout from "@/components/layout";
+import SEO from "@/components/seo";
 
 
 export default function Home() {
-  return (
-    <>
-      <Layout>
+    return (
+        <>
+            <SEO title={'Links'}/>
+            <Layout>
+                <Container maxW={'3xl'}>
+                    <Stack
+                        as={Box}
+                        textAlign={'center'}>
+                        <Center>
+                            <Avatar size={'xl'} src={'/me.png'}/>
+                        </Center>
+                        <Heading
+                            fontWeight={600}
+                            fontSize={{base: '2xl', sm: '3xl', md: '4xl'}}
+                            lineHeight={'110%'}>
+                            Hi, I&apos;m Hunter.
+                        </Heading>
+                        <Text color={'gray.500'}>
+                            Welcome to my little list of socials I use and more.
+                        </Text>
+                    </Stack>
 
-      </Layout>
-    </>
-  )
+                    {/* Social Links */}
+                    <Stack as={Box}
+                           textAlign={'center'}>
+
+                    </Stack>
+                </Container>
+            </Layout>
+        </>
+    )
 }
