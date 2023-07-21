589844ebead464aefb787ed6ab0b5d95080383c1
TheTeaCup <lw0482352@gmail.com>
2023-07-21 18:18:29 -0400

    Code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,34 @@
 import {
     Avatar,
     Center,
-    Divider,
     Heading,
     SimpleGrid,
     Tab,
     TabList,
-    TabPanel, TabPanels,
+    TabPanel,
+    TabPanels,
     Tabs,
     Text,
     VStack
 } from "@chakra-ui/react";
 import Layout from "@/components/layout";
-import Link from "next/link";
 import links from "@/data/links";
 import LinkCard from "@/components/linkCard";
 
 
 export default function Home() {
-  return (
-    <>
-        <Layout>
-            <Center>
-                <Avatar size={'xl'}/>
-            </Center>
-            <br/>
-            <VStack>
-                <Heading as="h1" size="2xl">Hunter Wilson</Heading>
-                <Text fontSize="xl">A list of links to my social media and other websites.</Text>
-            </VStack>
-            <br/>
+    return (
+        <>
+            <Layout>
+                <Center>
+                    <Avatar size={'xl'}/>
+                </Center>
+                <br/>
+                <VStack>
+                    <Heading as="h1" size="2xl">Hunter Wilson</Heading>
+                    <Text fontSize="xl">A list of links to my social media and other websites.</Text>
+                </VStack>
+                <br/>
 
 
                 <Tabs align={'center'} variant='soft-rounded' colorScheme='blue'>
@@ -69,7 +68,7 @@ export default function Home() {
                     </TabPanels>
                 </Tabs>
 
-        </Layout>
-    </>
-  )
+            </Layout>
+        </>
+    )
 }
